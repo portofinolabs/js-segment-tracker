4ec4fdf4baa96cd9b1b626e1375454041c06fc48
cdROma <38502733+gthrm@users.noreply.github.com>
2023-08-21 19:07:35 +0300

    feat: add an ability to change a page viewed event name

--- index.js
+++ index.js
@@ -2,83 +2,21 @@
  * Self-invoking function to initialize Segment tracking and intercept navigation changes.
  * @param {Window} global - The global window object.
  */
-;(function (global) {
-  let previousURL = window.location.href
+(function (global) {
+  const DATA_EVENT_PREFIX = 'data-event-';
+  const CURRENT_URL_PARAM_KEY = 'url';
 
-  /**
-   * Initializes Segment tracking for click events and navigation changes.
-   */
-  function initSegmentTracking() {
-    document.body.addEventListener("click", handleElementClick)
-    window.addEventListener("popstate", () => trackPageViewed(previousURL))
-    window.addEventListener("hashchange", () => trackPageViewed(previousURL))
-  }
-
+  let previousURL = window.location.href;
   function getTrackingDataFromEventTarget(target) {
-    let currentTarget = target
+    let currentTarget = target;
     while (currentTarget) {
-      const eventName = currentTarget.getAttribute("data-track-event")
+      const eventName = currentTarget.getAttribute('data-track-event');
       if (eventName) {
-        return eventName
+        return eventName;
       }
-      currentTarget = currentTarget.parentElement
+      currentTarget = currentTarget.parentElement;
     }
-    return null
-  }
-
-  /**
-   * Handles click events on DOM elements. Sends tracking data to Segment
-   * if the clicked element has a `data-track-event` attribute.
-   * @param {Event} event - The DOM click event.
-   */
-  function handleElementClick(event) {
-    const analytics = global.analytics || window.analytics
-    const eventName = getTrackingDataFromEventTarget(event.target)
-    console.log("eventName", eventName)
-    if (eventName) {
-      const eventData = {
-        ...getDataAttributesFromEventTarget(event.target),
-      }
-
-      if (analytics && typeof analytics.track === "function") {
-        analytics.track(eventName, eventData)
-      } else {
-        console.warn("Segment analytics is not available.")
-      }
-    }
-  }
-
-  const DATA_EVENT_PREFIX = 'data-event-'
-  const CURRENT_URL_PARAM_KEY = 'url'
-  function getSpecialAttributesFromEvent(key, value) {
-    switch (key) {
-      case CURRENT_URL_PARAM_KEY:
-        return !value ? previousURL : value
-      default:
-        return value
-    }
-  }
-
-  function getDataAttributesFromEventTarget(target) {
-    let currentTarget = target
-    const data = {}
-
-    while (currentTarget) {
-      for (let attr of currentTarget.attributes) {
-        if (attr.name.startsWith(DATA_EVENT_PREFIX)) {
-          const key = attr.name.slice(DATA_EVENT_PREFIX.length)
-          data[key] = getSpecialAttributesFromEvent(key, attr.value)
-        }
-      }
-
-      if (Object.keys(data).length > 0) {
-        return data
-      }
-
-      currentTarget = currentTarget.parentElement
-    }
-
-    return data
+    return null;
   }
 
   /**
@@ -89,11 +27,11 @@
   function gatherEventData(referrer = document.referrer) {
     return {
       path: window.location.pathname,
-      referrer: referrer,
+      referrer,
       search: window.location.search,
       title: document.title,
       url: window.location.href,
-    }
+    };
   }
 
   /**
@@ -101,40 +39,107 @@
    * @param {string} [referrer=document.referrer] - Referrer of the page.
    */
   function trackPageViewed(referrer = document.referrer) {
-    const eventData = gatherEventData(referrer)
-    const analytics = global.analytics || window.analytics
-    if (analytics && typeof analytics.track === "function") {
-      analytics.track("Page Viewed", eventData)
+    const eventData = gatherEventData(referrer);
+    const analytics = global.analytics || window.analytics;
+    if (analytics && typeof analytics.track === 'function') {
+      analytics.track(
+        process.env.PAGE_VIEWED_EVENT_NAME
+          || process.env.VITE_PAGE_VIEWED_EVENT_NAME
+          || 'Page Viewed',
+        eventData,
+      );
     } else {
-      console.warn("Segment analytics is not available for Page Viewed event.")
+      console.warn('Segment analytics is not available for Page Viewed event.');
     }
+  }
+
+  function getSpecialAttributesFromEvent(key, value) {
+    switch (key) {
+      case CURRENT_URL_PARAM_KEY:
+        return !value ? previousURL : value;
+      default:
+        return value;
+    }
+  }
+
+  function getDataAttributesFromEventTarget(target) {
+    let currentTarget = target;
+    const data = {};
+
+    while (currentTarget) {
+      for (const attr of currentTarget.attributes) {
+        if (attr.name.startsWith(DATA_EVENT_PREFIX)) {
+          const key = attr.name.slice(DATA_EVENT_PREFIX.length);
+          data[key] = getSpecialAttributesFromEvent(key, attr.value);
+        }
+      }
+
+      if (Object.keys(data).length > 0) {
+        return data;
+      }
+
+      currentTarget = currentTarget.parentElement;
+    }
+
+    return data;
+  }
+
+  /**
+   * Handles click events on DOM elements. Sends tracking data to Segment
+   * if the clicked element has a `data-track-event` attribute.
+   * @param {Event} event - The DOM click event.
+   */
+  function handleElementClick(event) {
+    const analytics = global.analytics || window.analytics;
+    const eventName = getTrackingDataFromEventTarget(event.target);
+
+    if (eventName) {
+      const eventData = {
+        ...getDataAttributesFromEventTarget(event.target),
+      };
+
+      if (analytics && typeof analytics.track === 'function') {
+        analytics.track(eventName, eventData);
+      } else {
+        console.warn('Segment analytics is not available.');
+      }
+    }
+  }
+
+  /**
+   * Initializes Segment tracking for click events and navigation changes.
+   */
+  function initSegmentTracking() {
+    document.body.addEventListener('click', handleElementClick);
+    window.addEventListener('popstate', () => trackPageViewed(previousURL));
+    window.addEventListener('hashchange', () => trackPageViewed(previousURL));
   }
 
   // Intercept changes to the history to track page views
   /**
    * Intercept history.pushState to track page views after a navigation change.
    */
-  const originalPushState = history.pushState
+  const originalPushState = history.pushState;
   history.pushState = function () {
-    originalPushState.apply(this, arguments)
+    originalPushState.apply(this, arguments);
     setTimeout(() => {
-      trackPageViewed(previousURL)
+      trackPageViewed(previousURL);
       // Update the previousURL after tracking the event.
-      previousURL = window.location.href
-    }, 0)
-  }
+      previousURL = window.location.href;
+    }, 0);
+  };
 
   /**
    * Intercept history.replaceState to track page views after a navigation change.
    */
-  const originalReplaceState = history.replaceState
+  const originalReplaceState = history.replaceState;
   history.replaceState = function () {
-    originalReplaceState.apply(this, arguments)
+    originalReplaceState.apply(this, arguments);
     setTimeout(() => {
-      trackPageViewed(previousURL)
-      previousURL = window.location.href
-    }, 0)
-  }
+      trackPageViewed(previousURL);
+      previousURL = window.location.href;
+    }, 0);
+  };
 
-  initSegmentTracking()
-})(typeof window !== "undefined" ? window : this)
+  initSegmentTracking();
+}(typeof window !== 'undefined' ? window : this));
